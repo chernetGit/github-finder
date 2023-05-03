e827f25ed9f7c2e5997b7b898a0840522c5a74df
chernetGit <chernettibebuyigzaw@gmail.com>
2023-05-03 12:28:06 +0300

    second commit

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 function Navbar(title) {
   return (
-    <nav class='navbar mb-12 shadow-lg bg-neutral text-neutral-content text-white bg-gray-700 '>
+    <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content text-white bg-gray-700 '>
       <div className='container mx-auto my-auto flex justify-between'>
         <div className='flex-none py-2'>
           <FaGithub className='inline pr-2 text-3xl' />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import UserResults from '../components/users/UserResults'
 
 function Home() {
   return (
-    <div>
-      <h1 className='text-6xl text-center'>Welcome</h1>
-    </div>
+    <>
+    {/* SEARCH COMPONENT */}
+      <UserResults/>
+      {/* <h1 className='text-6xl text-center'>Welcome</h1>
+      {process.env.REACT_APP_GITHUB_TOKEN} */}
+    </>
   )
 }
 
